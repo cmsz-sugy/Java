537efc4b893df41cb035d347a5aafc3f0ab58ebd
cmsz-sugy <sugy@chinamobilesz.com>
2017-08-26 23:03:08 +0800

    su2
    Signed-off-by:su3

--- src/java/Main.java
+++ src/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.print("hello world!");
-		System.out.print("hello world!");
+		System.out.print("hello XIANXIAN!");
+		
 
 	}
 
